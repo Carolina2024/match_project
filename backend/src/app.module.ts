b9a5947ae2d705b8f7db47f2e71cfbbf1a183507
Angel Añez <angdavid2910@gmail.com>
2025-04-26 15:56:32 -0400

    chore: add typeorm and env variables config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DB_CONFIG } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(DB_CONFIG),
+  ],
   controllers: [],
   providers: [],
 })

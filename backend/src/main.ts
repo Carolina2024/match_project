da05d431c67b5a8b647efbc432eda9a2deec1473
Angel Añez <angdavid2910@gmail.com>
2025-04-26 21:49:25 -0400

    refactor: create swagger.config.ts to centralize swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,32 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import { SWAGGER_OPTIONS } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //--------Documentanción de Swagger------------
-  const options = new DocumentBuilder()
-    .setTitle('Patas Pirque')
-    .setDescription(
-      `
-      Bienvenido a la documentación oficial de la API de Patas Pirque.
-
-      Aquí encontrarás la descripción detallada de los distintos endpoints disponibles, sus métodos, parámetros de entrada, formatos de respuesta y ejemplos de uso.
-
-      Te recomendamos seguir las instrucciones y ejemplos proporcionados para garantizar una correcta implementación y aprovechamiento de los servicios.
-      `,
-    )
-
-    .setVersion('1.0')
-    .addTag('default')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
-
   app.enableCors();
   app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, SWAGGER_OPTIONS);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }

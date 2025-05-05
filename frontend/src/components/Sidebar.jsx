b01b3e2fa431d89605c5a1c614a41e0120db99be
Carolina2024 <carcastillos@gmail.com>
2025-05-05 06:31:14 -0400

    feat: add edit option to adopter profile

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,10 @@
-import { FaFileAlt, FaUserFriends, FaUserEdit, FaThLarge, FaPaw } from "react-icons/fa";
+import {
+  FaFileAlt,
+  FaUserFriends,
+  FaUserEdit,
+  FaThLarge,
+  FaPaw,
+} from "react-icons/fa";
 
 const Sidebar = ({ onSelect, activeView }) => {
   const items = [
@@ -13,12 +19,16 @@ const Sidebar = ({ onSelect, activeView }) => {
     <div className="w-64 h-full bg-[#f5f5f5] p-6 border-r border-gray-300">
       {/* LOGO Y NOMBRE */}
       <div className="flex items-center gap-3 mb-6">
-  <img src="src/assets/logo.png" alt="Logo" className="w-[60px] h-[60px] rounded-full" />
-  <div className="leading-tight text-left">
-    <p className="text-md font-bold text-gray-700">Patas</p>
-    <p className="text-sm text-gray-500">Pirque</p>
-  </div>
-</div>
+        <img
+          src="src/assets/logo.png"
+          alt="Logo"
+          className="w-[60px] h-[60px] rounded-full"
+        />
+        <div className="leading-tight text-left">
+          <p className="text-md font-bold text-gray-700">Patas</p>
+          <p className="text-sm font-bold text-gray-700">Pirque</p>
+        </div>
+      </div>
 
       {/* MENÚ DE NAVEGACIÓN */}
       <nav className="space-y-4">
@@ -29,11 +39,12 @@ const Sidebar = ({ onSelect, activeView }) => {
             className={`flex items-center gap-3 px-4 py-2 rounded-lg w-full text-left font-medium transition-colors duration-200
               ${
                 activeView === item.label
-                  ? "bg-[#f4a470] text-white"
+                  ? "bg-[#FAAA75] text-gray-700 border-t border-l border-r border-[#595146] border-b-[3px] border-b-[#595146]"
                   : "text-gray-700 hover:bg-orange-100"
               }`}
           >
-            {item.icon}
+            {/* Iconos con color gris */}
+            <span className="text-gray-500">{item.icon}</span>
             {item.label}
           </button>
         ))}

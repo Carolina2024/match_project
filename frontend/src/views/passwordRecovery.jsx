9b0a838fbe24580647727da4deefdcd33066e40e
Luis Angel <angel.quispe.navarro@outlook.com>
2025-05-07 21:37:09 -0500

    Update Modal Password

--- frontend/src/views/passwordRecovery.jsx
+++ frontend/src/views/passwordRecovery.jsx
@@ -92,14 +92,14 @@ export default PasswordRecovery;*/
 
 //PAGINA DOS******************************************************************************
 
-import  { useState } from 'react';
-//import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function PasswordRecovery() {
-  const [codigo, setCodigo] = useState(['', '', '', '', '', '']);
-  const [nuevaContrasena, setNuevaContrasena] = useState('');
-  const [confirmarContrasena, setConfirmarContrasena] = useState('');
+  const [codigo, setCodigo] = useState(["", "", "", "", "", ""]);
+  const [nuevaContrasena, setNuevaContrasena] = useState("");
+  const [confirmarContrasena, setConfirmarContrasena] = useState("");
   const [errores, setErrores] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleCodigoChange = (index, value) => {
     if (/^[0-9]?$/.test(value)) {
@@ -111,19 +111,16 @@ function PasswordRecovery() {
 
   const validarFormulario = () => {
     const nuevosErrores = {};
-
-    if (codigo.some((digito) => digito === '')) {
-      nuevosErrores.codigo = 'Completa todos los dígitos del código';
+    if (codigo.some((digito) => digito === "")) {
+      nuevosErrores.codigo = "Completa todos los dígitos del código";
     }
-
-    if (nuevaContrasena.length < 6) {
-      nuevosErrores.nuevaContrasena = 'La contraseña debe tener al menos 8 caracteres';
+    if (nuevaContrasena.length < 8) {
+      nuevosErrores.nuevaContrasena =
+        "La contraseña debe tener al menos 8 caracteres";
     }
-
     if (nuevaContrasena !== confirmarContrasena) {
-      nuevosErrores.confirmarContrasena = 'Las contraseñas no coinciden';
+      nuevosErrores.confirmarContrasena = "Las contraseñas no coinciden";
     }
-
     setErrores(nuevosErrores);
     return Object.keys(nuevosErrores).length === 0;
   };
@@ -131,38 +128,36 @@ function PasswordRecovery() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validarFormulario()) {
-      const codigoCompleto = codigo.join('');
-      console.log('Código:', codigoCompleto);
-      console.log('Nueva contraseña:', nuevaContrasena);
-      
+      const codigoCompleto = codigo.join("");
+      console.log("Código:", codigoCompleto);
+      console.log("Nueva contraseña:", nuevaContrasena);
     }
-
-   // const navigate = useNavigate();
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm">
-      <div className="relative bg-secundary p-8 rounded-3xl shadow-lg w-full max-w-md text-center">
+    <div className="absolute top-0 left-0 w-full z-50 bg-black/50 backdrop-blur-[4px] px-4 py-10 flex justify-center pt-24">
+      <div className="relative bg-[#FCFCF7] w-full max-w-md min-h-screen rounded-3xl shadow-xl flex flex-col items-center border border-[#CBCBCB] px-6 pb-10 pt-24">
+        {/* Logo flotante */}
         <div className="absolute -top-12 left-1/2 transform -translate-x-1/2">
           <img
             src="src/assets/logo.png"
             alt="Logo Patas Pirque"
-            className="w-24 h-24 rounded-full object-cover"
+            className="w-24 h-24 rounded-full object-containt drop-shadow-lg"
           />
         </div>
 
-        <h2 className="mt-12 text-2xl font-bold font-secundary mb-2 text-[#595146]">
+        <h2 className="text-lg font-bold mb-2 text-center font-secundary text-[#393939]">
           Recuperar contraseña
         </h2>
-        <p className="text-base text-[#767575] mb-6">
-          Ingresa tu correo electrónico para restablecer tu contraseña.
+        <p className="text-sm text-[#767575] mb-6 text-center">
+          Ingresa el código y define una nueva contraseña para tu cuenta.
         </p>
 
-        <div className="mb-6 ml-2">
-          <label className="block text-base font-semibold text-left text-[#595146] mb-2">
+        <div className="mb-6 w-full">
+          <label className="block text-sm font-semibold text-left text-[#595146] mb-2">
             Código de recuperación
           </label>
-          <div className="flex gap-3">
+          <div className="flex justify-center gap-2">
             {codigo.map((valor, i) => (
               <input
                 key={i}
@@ -170,61 +165,89 @@ function PasswordRecovery() {
                 maxLength="1"
                 value={valor}
                 onChange={(e) => handleCodigoChange(i, e.target.value)}
-                className="w-12 h-14 border border-[#76757580] rounded text-center text-lg"
+                className="w-10 h-12 border border-[#76757580] rounded text-center text-lg focus:outline-none focus:border-1 focus:border-primary"
               />
             ))}
           </div>
-          {errores.codigo && <p className="text-red-500 text-sm mt-1">{errores.codigo}</p>}
+          <p className="text-xs text-[#A4A4A4] text-center mt-2">
+            El código será validado automáticamente.
+          </p>
+          {errores.codigo && (
+            <p className="text-red-500 text-sm mt-1 text-center">
+              {errores.codigo}
+            </p>
+          )}
         </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-base font-semibold text-[#595146] mb-2 text-left">
+        <form onSubmit={handleSubmit} className="w-full space-y-5">
+          <div>
+            <label className="block text-sm font-semibold text-[#595146] mb-2">
               Nueva contraseña
             </label>
             <div className="relative">
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 value={nuevaContrasena}
                 onChange={(e) => setNuevaContrasena(e.target.value)}
-                className="w-full px-4 py-2 bg-[#FFFFFFBD] border border-primary rounded-3xl text-sm"
+                className="w-full px-4 py-2 bg-white border border-primary rounded-full text-sm"
               />
-              {errores.nuevaContrasena && <p className="text-red-500 text-sm mt-1">{errores.nuevaContrasena}</p>}
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-4 flex items-center cursor-pointer"
+              >
+                👁️
+              </span>
+              {errores.nuevaContrasena && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errores.nuevaContrasena}
+                </p>
+              )}
             </div>
           </div>
 
-          <div className="mb-8">
-            <label className="block text-base font-semibold text-[#595146] mb-2 text-left">
+          <div>
+            <label className="block text-sm font-semibold text-[#595146] mb-2">
               Confirmar contraseña
             </label>
             <div className="relative">
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 value={confirmarContrasena}
                 onChange={(e) => setConfirmarContrasena(e.target.value)}
-                className="w-full px-4 py-2 border bg-[#FFFFFFBD] border-primary rounded-3xl text-sm"
+                className="w-full px-4 py-2 bg-white border border-primary rounded-full text-sm"
               />
-              {errores.confirmarContrasena && <p className="text-red-500 text-sm mt-1">{errores.confirmarContrasena}</p>}
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-4 flex items-center cursor-pointer"
+              >
+                👁️
+              </span>
+              {errores.confirmarContrasena && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errores.confirmarContrasena}
+                </p>
+              )}
             </div>
           </div>
 
-          <button
-            type="submit"
-            className="py-2 px-6 bg-primary text-[#FFFFFF] text-base font-semibold rounded-full shadow-[#00000040] shadow-md">
-            Restablecer contraseña
-          </button>
+          <div className="flex flex-col gap-4 items-center mt-6">
+            <button
+              type="submit"
+              className="w-auto px-7 text-lg py-2 bg-primary text-white font-semibold rounded-full shadow-md transition"
+            >
+              Restablecer contraseña
+            </button>
+            <button
+              type="button"
+              className="w-auto px-6 text-lg py-1 border-2 border-primary text-primary font-semibold rounded-full"
+            >
+              Volver atrás
+            </button>
+          </div>
         </form>
-
-        <button
-          type="button"
-          //onClick={() => navigate(-1)}
-          className="mt-4 border-2 rounded-full py-2 px-6 font-semibold text-base text-primary">
-          Volver atrás
-        </button>
       </div>
     </div>
   );
 }
 
 export default PasswordRecovery;
-

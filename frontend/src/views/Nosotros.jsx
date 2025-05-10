69218a8f15f5d44984aab5b15280132762563bcd
paola chaves <paochavess22@gmail.com>
2025-05-09 22:26:28 -0500

    Ajuste de detalles de margin

--- frontend/src/views/Nosotros.jsx
+++ frontend/src/views/Nosotros.jsx
@@ -9,18 +9,18 @@ import nosotrosp5 from "../assets/nosotrosp5.png";
 
 const Nosotros = () => {
   return (
-    <main className="md:max-w-6xl max-w-lg mx-20 bg-[#F9F9F9] flex flex-col justify-center items-center rounded-t-[50px]">
+    <main className="w-full bg-[#F9F9F9] flex flex-col justify-center items-center rounded-t-[50px] pb-20 ">
       {/* Imagen encabezado */}
-      <div className="relative w-full md:max-w-6xl max-w-lg">
+      <div className="w-full ">
         <img
           src={nosotros}
           alt="Gato acariciado"
-          className="w-full h-[500px]  object-cover rounded-t-[50px]"
+          className="w-full h-[250px] md:h-[400px] lg:h-[500px] object-cover rounded-t-[50px]"
         />
       </div>
 
       {/* Nuestra historia */}
-      <div className="max-w-4xl  border-1 border-primary rounded-[50px] mt-12 mx-auto py-8 px-4">
+      <div className="w-full max-w-5xl px-8 py-8 md:py-12  border-1 border-primary rounded-[50px] mt-12 mx-auto">
         <h2 className="text-3xl pl-14 font-bold text-primary mb-4">
           Nuestra historia
         </h2>
@@ -35,56 +35,7 @@ const Nosotros = () => {
         </p>
       </div>
 
-      {/* Estadísticas y fundadoras */}
-      <div className="max-w-4xl justify-center mx-auto px-4 py-8 relative">
-        <div className="max-w-4xl mx-auto px-4 py-8">
-          <div className="flex flex-col gap-6">
-            {/* Bloque 1 */}
-            <div className="flex items-center justify-center gap-4">
-              <svg
-                width="61"
-                height="61"
-                viewBox="0 0 61 61"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M24.591 35.6904L26.8149 28.4417L20.9691 23.7364H28.2128L30.5003 16.6149L32.7878 23.7364H40.0316L34.1222 28.4417L36.3462 35.6904L30.5003 31.1759L24.591 35.6904ZM15.2503 59.344V39.6962C13.6406 37.9159 12.391 35.8811 11.5014 33.5921C10.6118 31.303 10.167 28.8656 10.167 26.2798C10.167 20.5996 12.1368 15.7883 16.0764 11.846C20.016 7.90375 24.8239 5.93262 30.5003 5.93262C36.1767 5.93262 40.9847 7.90375 44.9243 11.846C48.8639 15.7883 50.8337 20.5996 50.8337 26.2798C50.8337 28.8656 50.3889 31.303 49.4993 33.5921C48.6097 35.8811 47.3601 37.9159 45.7503 39.6962V59.344L30.5003 54.2572L15.2503 59.344ZM30.5003 41.5402C34.7364 41.5402 38.3371 40.0566 41.3024 37.0893C44.2677 34.122 45.7503 30.5188 45.7503 26.2798C45.7503 22.0408 44.2677 18.4377 41.3024 15.4704C38.3371 12.5031 34.7364 11.0194 30.5003 11.0194C26.2642 11.0194 22.6635 12.5031 19.6982 15.4704C16.733 18.4377 15.2503 22.0408 15.2503 26.2798C15.2503 30.5188 16.733 34.122 19.6982 37.0893C22.6635 40.0566 26.2642 41.5402 30.5003 41.5402ZM20.3337 51.7774L30.5003 49.1704L40.667 51.7774V43.8929C39.1844 44.7407 37.5852 45.4083 35.8696 45.8958C34.154 46.3833 32.3642 46.627 30.5003 46.627C28.6364 46.627 26.8467 46.3833 25.1311 45.8958C23.4154 45.4083 21.8163 44.7407 20.3337 43.8929V51.7774Z"
-                  fill="#F4A470"
-                />
-              </svg>
-              <p>
-                <span className="text-4xl font-bold text-primary">500</span>{" "}
-                <span className="font-bold  text-[#595146] text-xl">
-                  mascotas rescatadas
-                </span>
-              </p>
-            </div>
-
-            {/* Bloque 2 */}
-            <div className="flex items-center justify-center gap-4">
-              <svg
-                width="61"
-                height="61"
-                viewBox="0 0 61 61"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M24.591 35.6904L26.8149 28.4417L20.9691 23.7364H28.2128L30.5003 16.6149L32.7878 23.7364H40.0316L34.1222 28.4417L36.3462 35.6904L30.5003 31.1759L24.591 35.6904ZM15.2503 59.344V39.6962C13.6406 37.9159 12.391 35.8811 11.5014 33.5921C10.6118 31.303 10.167 28.8656 10.167 26.2798C10.167 20.5996 12.1368 15.7883 16.0764 11.846C20.016 7.90375 24.8239 5.93262 30.5003 5.93262C36.1767 5.93262 40.9847 7.90375 44.9243 11.846C48.8639 15.7883 50.8337 20.5996 50.8337 26.2798C50.8337 28.8656 50.3889 31.303 49.4993 33.5921C48.6097 35.8811 47.3601 37.9159 45.7503 39.6962V59.344L30.5003 54.2572L15.2503 59.344ZM30.5003 41.5402C34.7364 41.5402 38.3371 40.0566 41.3024 37.0893C44.2677 34.122 45.7503 30.5188 45.7503 26.2798C45.7503 22.0408 44.2677 18.4377 41.3024 15.4704C38.3371 12.5031 34.7364 11.0194 30.5003 11.0194C26.2642 11.0194 22.6635 12.5031 19.6982 15.4704C16.733 18.4377 15.2503 22.0408 15.2503 26.2798C15.2503 30.5188 16.733 34.122 19.6982 37.0893C22.6635 40.0566 26.2642 41.5402 30.5003 41.5402ZM20.3337 51.7774L30.5003 49.1704L40.667 51.7774V43.8929C39.1844 44.7407 37.5852 45.4083 35.8696 45.8958C34.154 46.3833 32.3642 46.627 30.5003 46.627C28.6364 46.627 26.8467 46.3833 25.1311 45.8958C23.4154 45.4083 21.8163 44.7407 20.3337 43.8929V51.7774Z"
-                  fill="#F4A470"
-                />
-              </svg>
-              <p>
-                <span className="text-4xl font-bold text-primary">230</span>{" "}
-                <span className="font-bold  text-[#595146] text-xl">
-                  mascotas adoptadas
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-white rounded-lg border-[#DFDFDF] mt-24 border-1 p-6 text-center">
+      <div className="bg-white rounded-lg border-[#DFDFDF] mt-35 border-1 p-5 text-center">
         {/* Contenedor para la imagen */}
         <div className="relative -mt-25 px-10 mb-4">
           <img
@@ -102,8 +53,8 @@ const Nosotros = () => {
       </div>
 
       {/* Contenedor para el texto principal (si es necesario un contenedor separado) */}
-      <div className="mx-14">
-        <p className="font-medium text-xl text-[#000000] p-20 text-justify">
+      <div className=" w-full max-w-5xl px-1 py-6">
+        <p className="font-medium text-xl text-[#000000] px-20 py-10 text-justify">
           “Siempre tuve una conexión especial con los animales. <br />
           Desde niña rescataba animales, guiada solo por mi instinto y cariño.{" "}
           <br />
@@ -124,18 +75,52 @@ const Nosotros = () => {
         </p>
       </div>
 
+      {/* Estadísticas y fundadoras */}
+      <div className="flex items-center gap-2 sm:gap-4 lg:gap-6 mb-10 sm:mb-14 lg:mb-16">
+        <svg
+          width="61"
+          height="61"
+          viewBox="0 0 61 61"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <mask
+           id="uniqueMask"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="61"
+            height="61">
+            <rect width="61" 
+            height="61" fill="#D9D9D9" />
+          </mask>
+          <g mask="url(#uniqueMask)">
+            <path
+              d="M24.591 35.6904L26.8149 28.4417L20.9691 23.7364H28.2128L30.5003 16.6149L32.7878 23.7364H40.0316L34.1222 28.4417L36.3462 35.6904L30.5003 31.1759L24.591 35.6904ZM15.2503 59.344V39.6962C13.6406 37.9159 12.391 35.8811 11.5014 33.5921C10.6118 31.303 10.167 28.8656 10.167 26.2798C10.167 20.5996 12.1368 15.7883 16.0764 11.846C20.016 7.90375 24.8239 5.93262 30.5003 5.93262C36.1767 5.93262 40.9847 7.90375 44.9243 11.846C48.8639 15.7883 50.8337 20.5996 50.8337 26.2798C50.8337 28.8656 50.3889 31.303 49.4993 33.5921C48.6097 35.8811 47.3601 37.9159 45.7503 39.6962V59.344L30.5003 54.2572L15.2503 59.344ZM30.5003 41.5402C34.7364 41.5402 38.3371 40.0566 41.3024 37.0893C44.2677 34.122 45.7503 30.5188 45.7503 26.2798C45.7503 22.0408 44.2677 18.4377 41.3024 15.4704C38.3371 12.5031 34.7364 11.0194 30.5003 11.0194C26.2642 11.0194 22.6635 12.5031 19.6982 15.4704C16.733 18.4377 15.2503 22.0408 15.2503 26.2798C15.2503 30.5188 16.733 34.122 19.6982 37.0893C22.6635 40.0566 26.2642 41.5402 30.5003 41.5402ZM20.3337 51.7774L30.5003 49.1704L40.667 51.7774V43.8929C39.1844 44.7407 37.5852 45.4083 35.8696 45.8958C34.154 46.3833 32.3642 46.627 30.5003 46.627C28.6364 46.627 26.8467 46.3833 25.1311 45.8958C23.4154 45.4083 21.8163 44.7407 20.3337 43.8929V51.7774Z"
+              fill="#F4A470"
+            />
+          </g>
+        </svg>
+
+        <span className="text-primary text-2xl sm:text-3xl lg:text-4xl font-bold">
+          + 230{" "}
+        </span>
+        <span className="text-lg sm:text-xl lg:text-2xl font-bold text-[#595146]">
+          perros y gatos rescatados
+        </span>
+      </div>
+
       {/* Imagen grande */}
-      <div className="max-w-full ">
+      <div className="w-full">
         <img
           src={nosotros2}
           alt="Veterinaria y perro"
-          className="w-full h-[500px] rounded-lg"
+          className="w-full h-[250px] md:h-[400px] lg:h-[500px] object-cover rounded-lg"
         />
       </div>
 
-      <div className="max-w-6xl mt-2 py-8">
-        <div className="bg-[#F9F9F9] rounded-[50px] mx-16 px-24 py-6 border-1   border-primary">
-          <h3 className="text-3xl font-bold text-primary mb-4">
+      <div className="w-full max-w-6xl px-6 py-8">
+        <div className="bg-[#F9F9F9] rounded-[30px] md:rounded-[50px] px-6 md:px-24 py-6 border border-primary">
+          <h3 className="text-2xl md:text-3xl font-bold text-primary mb-4">
             Nuestro propósito
           </h3>
           <p className="text-[#000000] font-medium text-xl mb-4">
@@ -150,15 +135,14 @@ const Nosotros = () => {
       </div>
 
       {/* Misión y Visión */}
-      <div className=" ">
+      <div className=" w-full max-w-6xl px-6 py-8">
         {" "}
         {/* Contenedor padre */}
-        <div className="grid md:grid-cols-2 gap-4 bg-[#FFFFFF] rounded-[50px] mx-16 px-24 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 bg-white rounded-[30px] md:rounded-[50px] px-6 md:px-24 py-6">
           {" "}
           {/* Caja de Misión */}
           <div className="p-4">
             {" "}
-            {/* Añadí un padding para el contenido interno */}
             <div className="flex items-center gap-2">
               <svg
                 width="34"
@@ -175,7 +159,6 @@ const Nosotros = () => {
             </div>
             <p className="text-xl text-[#000000] font-medium mt-2">
               {" "}
-              {/* Añadí un margen superior */}
               Rescatar, rehabilitar y <br /> reubicar perros en <br /> situación
               de abandono,
               <br />
@@ -187,7 +170,6 @@ const Nosotros = () => {
           {/* Caja de Visión */}
           <div className="p-4">
             {" "}
-            {/* Añadí un padding para el contenido interno */}
             <div className="flex items-center gap-2">
               <svg
                 width="34"
@@ -202,22 +184,20 @@ const Nosotros = () => {
               </svg>
               <h4 className="text-primary text-3xl font-bold">Visión</h4>
             </div>
-            <p className="text-xl font-medium text-[#000000] mt-2">
-              {" "}
-              {/* Añadí un margen superior */}
-              ° Amor y respeto por los seres vivos <br />
-              ° Educación como motor de cambio <br />
-              ° Solidaridad y compromiso social <br />
-              ° Vida rural en armonía con la naturaleza <br />° Responsabilidad
-              en la adopción y tenencia animal
-            </p>
+            <ul className="text-xl font-medium text-[#000000] mt-2 list-disc list-inside">
+              <li>Amor y respeto por los seres vivos </li>
+              <li>Educación como motor de cambio </li>
+              <li> Solidaridad y compromiso social </li>
+              <li>Vida rural en armonía con la naturaleza</li>
+              <li>Responsabilidad en la adopción y tenencia animal</li>
+            </ul>
           </div>
         </div>
       </div>
 
       {/* ¿Cómo ayudarnos? */}
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold font-secundary text-primary mt-38 text-center mb-8">
+        <h2 className="text-3xl font-bold font-secundary text-primary mt-20 text-center mb-8">
           ¿Cómo colaborar?
           <hr className="border-primary mt-4" />
         </h2>
@@ -298,13 +278,12 @@ const Nosotros = () => {
 
             {/* Corazón Quiltro */}
             <div className="rounded-[50px] border-1 border-primary p-6 flex flex-col items-center">
-              <div className="w-68 h-52 rounded-[50px] overflow-hidden mb-4 bg-gray-300 flex items-center justify-center">
+              <div className="w-68 h-52 rounded-[50px] overflow-hidden mb-4 bg-gray-300">
                 <img
                   src={nosotrosp3}
                   alt="Huella Fuerte"
                   className="w-full h-full object-cover"
                 />
-                <div className="w-24 h-24 bg-gray-300 rounded-md" />
               </div>
               <h4 className="font-bold text-primary text-xl mb-1">
                 Corazón Quiltro
@@ -320,10 +299,10 @@ const Nosotros = () => {
         </div>
 
         {/* Campañas y Donaciones */}
-        <div className="flex justify-center mt-30">
+        <div className="flex justify-center mt-20">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-14">
             {/* Campañas */}
-            <div className="rounded-[50px] border-1 border-primary bg-[#FFFFFF] py-6 px-2 flex flex-col items-center">
+            <div className="rounded-[50px] border-1 border-primary bg-[#FFFFFF] p-6 flex flex-col items-center">
               <h3 className="text-3xl font-bold text-primary mb-4 flex items-center">
                 <svg
                   width="30"
@@ -338,7 +317,7 @@ const Nosotros = () => {
                 </svg>
                 Campañas
               </h3>
-              <div className="w-62 h-50 rounded-[50px] overflow-hidden mb-4 bg-gray-100">
+              <div className="w-68 h-50 rounded-[50px] overflow-hidden mb-4 bg-gray-100">
                 <img
                   src={nosotrosg4}
                   alt="Gatos Ferales en Rescate"
@@ -346,7 +325,7 @@ const Nosotros = () => {
                 />
               </div>
               <h4 className="font-semibold text-primary mb-1 text-xl text-center">
-                Gatos Ferales en Rescate
+                Gatos Ferales en <br /> Rescate
               </h4>
               <button className="bg-primary text-white mt-3 rounded-full px-8 py-1 font-bold text-xl">
                 Apoyar
@@ -357,7 +336,7 @@ const Nosotros = () => {
             <div className="rounded-[50px] border-1 border-primary bg-[#FFFFFF] p-6 flex flex-col items-center">
               <h3 className="text-3xl font-bold text-primary mb-4 flex items-center">
                 <svg
-                 className="mr-2 inline-block align-middle"
+                  className="mr-2 inline-block align-middle"
                   width="31"
                   height="31"
                   viewBox="0 5 31 31"
@@ -370,20 +349,18 @@ const Nosotros = () => {
                     y="0"
                     width="30"
                     height="30">
-                    <rect width="30" 
-                    height="30" 
-                    fill="#f4a470" />
+                    <rect width="30" height="30" fill="#f4a470" />
                   </mask>
                   <g mask="url(#mask0_1624_3026)">
                     <path
                       d="M20 18.2652L14.8125 13.1993C14.1667 12.5739 13.6198 11.8807 13.1719 11.1197C12.724 10.3588 12.5 9.53012 12.5 8.63367C12.5 7.48706 12.901 6.51243 13.7031 5.7098C14.5052 4.90717 15.4792 4.50586 16.625 4.50586C17.2917 4.50586 17.9167 4.64658 18.5 4.92802C19.0833 5.20946 19.5833 5.58993 20 6.06942C20.4167 5.58993 20.9167 5.20946 21.5 4.92802C22.0833 4.64658 22.7083 4.50586 23.375 4.50586C24.5208 4.50586 25.4948 4.90717 26.2969 5.7098C27.099 6.51243 27.5 7.48706 27.5 8.63367C27.5 9.53012 27.2812 10.3588 26.8438 11.1197C26.4062 11.8807 25.8646 12.5739 25.2188 13.1993L20 18.2652ZM20 14.7628L23.4062 11.4168C23.8021 11.0207 24.1667 10.5986 24.5 10.1503C24.8333 9.70211 25 9.19656 25 8.63367C25 8.17503 24.8438 7.78935 24.5312 7.47663C24.2188 7.16392 23.8333 7.00756 23.375 7.00756C23.0833 7.00756 22.8073 7.06489 22.5469 7.17956C22.2865 7.29422 22.0625 7.46621 21.875 7.69553L20 9.94707L18.125 7.69553C17.9375 7.46621 17.7135 7.29422 17.4531 7.17956C17.1927 7.06489 16.9167 7.00756 16.625 7.00756C16.1667 7.00756 15.7812 7.16392 15.4688 7.47663C15.1562 7.78935 15 8.17503 15 8.63367C15 9.19656 15.1667 9.70211 15.5 10.1503C15.8333 10.5986 16.1979 11.0207 16.5938 11.4168L20 14.7628ZM8.75 25.1449L17.4375 27.5215L24.875 25.2075C24.7708 25.0198 24.6198 24.8583 24.4219 24.7228C24.224 24.5872 24 24.5195 23.75 24.5195H17.4375C16.875 24.5195 16.4271 24.4986 16.0938 24.457C15.7604 24.4153 15.4167 24.3319 15.0625 24.2068L12.1562 23.2374L12.8438 20.7982L15.375 21.6425C15.7292 21.7468 16.1458 21.8302 16.625 21.8927C17.1042 21.9552 17.8125 21.9969 18.75 22.0178C18.75 21.7885 18.6823 21.5696 18.5469 21.3611C18.4115 21.1526 18.25 21.0171 18.0625 20.9546L10.75 18.2652H8.75V25.1449ZM1.25 29.5229V15.7635H10.75C10.8958 15.7635 11.0417 15.7792 11.1875 15.8104C11.3333 15.8417 11.4688 15.8782 11.5938 15.9199L18.9375 18.6405C19.625 18.8907 20.1823 19.3285 20.6094 19.9539C21.0365 20.5793 21.25 21.2673 21.25 22.0178H23.75C24.7917 22.0178 25.6771 22.3618 26.4062 23.0497C27.1354 23.7377 27.5 24.6446 27.5 25.7703V27.0212L17.5 30.1483L8.75 27.7092V29.5229H1.25ZM3.75 27.0212H6.25V18.2652H3.75V27.0212Z"
-                      fill="#F4A470"
+                      fill="currentColor"
                     />
                   </g>
                 </svg>
                 Donaciones
               </h3>
-              <div className="w-62 h-50 rounded-[50px] overflow-hidden mb-4 bg-gray-100">
+              <div className="w-68 h-50 rounded-[50px] overflow-hidden mb-4 bg-gray-100">
                 <img
                   src={nosotrosp5}
                   alt="Para comida, medicamentos, etc."
@@ -391,7 +368,7 @@ const Nosotros = () => {
                 />
               </div>
               <h4 className="font-semibold text-primary mb-1 text-xl text-center">
-                Para comida, medicamentos, etc.
+                Para comida, <br /> medicamentos, etc.
               </h4>
               <button className="bg-primary text-white mt-3 rounded-full px-8 py-1 font-bold text-xl">
                 Apoyar

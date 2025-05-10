aebcc1a04c8571b17e2885d8a84eb9045296367f
Luis Angel <angel.quispe.navarro@outlook.com>
2025-05-10 14:42:30 -0500

    Logic navbar of login or not, views options

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,10 +1,20 @@
 import { useState, useRef } from "react";
-import { Link } from "react-router-dom";
-import { Menu, X, ChevronDown } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Menu, X, ChevronDown, User } from "lucide-react";
 import logo from "../assets/logo.png";
 import AuthModalsController from "../components/modals/AuthModalsController";
 
 const Navbar = () => {
+  const stored = localStorage.getItem("user");
+  const user = stored ? JSON.parse(stored) : null;
+  const loggedIn = Boolean(user);
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+
   // Menú general
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => setMenuOpen(!menuOpen);
@@ -18,26 +28,33 @@ const Navbar = () => {
   const [isInicioOpen, setIsInicioOpen] = useState(false);
   const hideTimeoutInicio = useRef(null);
   const handleMouseEnterInicio = () => {
-    if (hideTimeoutInicio.current) clearTimeout(hideTimeoutInicio.current);
+    clearTimeout(hideTimeoutInicio.current);
     setIsInicioOpen(true);
   };
   const handleMouseLeaveInicio = () => {
-    hideTimeoutInicio.current = setTimeout(() => {
-      setIsInicioOpen(false);
-    }, 150);
+    hideTimeoutInicio.current = setTimeout(() => setIsInicioOpen(false), 150);
   };
 
-  // Dropdown Nosotros
+  // Dropdown “Nosotros”
   const [isNosotrosOpen, setIsNosotrosOpen] = useState(false);
   const hideTimeoutNos = useRef(null);
   const handleMouseEnterNosotros = () => {
-    if (hideTimeoutNos.current) clearTimeout(hideTimeoutNos.current);
+    clearTimeout(hideTimeoutNos.current);
     setIsNosotrosOpen(true);
   };
   const handleMouseLeaveNosotros = () => {
-    hideTimeoutNos.current = setTimeout(() => {
-      setIsNosotrosOpen(false);
-    }, 150);
+    hideTimeoutNos.current = setTimeout(() => setIsNosotrosOpen(false), 150);
+  };
+
+  // Dropdown “Usuario”
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+  const hideTimeoutUser = useRef(null);
+  const handleMouseEnterUser = () => {
+    clearTimeout(hideTimeoutUser.current);
+    setIsUserMenuOpen(true);
+  };
+  const handleMouseLeaveUser = () => {
+    hideTimeoutUser.current = setTimeout(() => setIsUserMenuOpen(false), 150);
   };
 
   return (
@@ -61,36 +78,45 @@ const Navbar = () => {
 
       {/* Desktop */}
       <nav className="hidden md:flex items-center gap-8 text-lg font-normal text-black font-primary">
-        {/* — INICIO con Dropdown — */}
-        <div
-          className="relative"
-          onMouseEnter={handleMouseEnterInicio}
-          onMouseLeave={handleMouseLeaveInicio}
-        >
-          <div className="flex items-center cursor-pointer hover:text-primary transition">
-            <span>Inicio</span>
-            <ChevronDown size={16} className="ml-1 stroke-[#767575]" />
+        {/* Inicio sin Logueo y dropdown cuando se loguea */}
+        {loggedIn ? (
+          <div
+            className="relative"
+            onMouseEnter={handleMouseEnterInicio}
+            onMouseLeave={handleMouseLeaveInicio}
+          >
+            <Link
+              to="/"
+              className="flex items-center cursor-pointer hover:text-primary transition"
+            >
+              <span>Inicio</span>
+              <ChevronDown size={16} className="ml-1 stroke-[#767575]" />
+            </Link>
+            {isInicioOpen && (
+              <div className="absolute top-full left-0 mt-2 w-52 bg-white rounded-lg shadow-lg flex flex-col text-center z-50">
+                <Link
+                  to="/#seguimiento"
+                  className="px-4 py-2 text-sm hover:text-primary transition"
+                >
+                  Seguimiento de tu match
+                </Link>
+                <Link
+                  to="/#historias"
+                  className="px-4 py-2 text-sm hover:text-primary transition"
+                >
+                  Historias de adopción
+                </Link>
+              </div>
+            )}
           </div>
-          {isInicioOpen && (
-            <div className="absolute top-full left-0  text-center mt-2 w-52 bg-white rounded-lg shadow-lg flex flex-col z-50">
-              <Link
-                to="/#seguimiento"
-                className="px-4 py-2 text-sm font-normal font-tertiary hover:text-primary transition"
-              >
-                Seguimiento de tu match
-              </Link>
-              <Link
-                to="/#historias"
-                className="px-4 py-2 text-sm font-normal font-tertiary hover:text-primary transition"
-              >
-                Historias de adopción
-              </Link>
-            </div>
-          )}
-        </div>
+        ) : (
+          <Link to="/" className="hover:text-primary transition">
+            Inicio
+          </Link>
+        )}
         <span className="border-r border-2 h-10 border-primary" />
 
-        {/* Nosotros */}
+        {/* Nosotros con dropdown */}
         <div
           className="relative"
           onMouseEnter={handleMouseEnterNosotros}
@@ -101,22 +127,22 @@ const Navbar = () => {
             <ChevronDown size={16} className="ml-1 stroke-[#767575]" />
           </div>
           {isNosotrosOpen && (
-            <div className="absolute top-full text-center left-0 mt-2 w-48 bg-white rounded-lg shadow-lg flex flex-col z-50">
+            <div className="absolute top-full left-0 mt-2 w-48 bg-white rounded-lg shadow-lg flex flex-col text-center z-50">
               <Link
                 to="/Nosotros#historia"
-                className="px-4 py-2 text-sm font-normal font-tertiary hover:text-primary transition"
+                className="px-4 py-2 text-sm hover:text-primary transition"
               >
                 Historia del refugio
               </Link>
               <Link
                 to="/Nosotros#proposito"
-                className="px-4 py-2 text-sm font-normal font-tertiary hover:text-primary transition"
+                className="px-4 py-2 text-sm hover:text-primary transition"
               >
                 Nuestro propósito
               </Link>
               <Link
                 to="/Nosotros#colaborar"
-                className="px-4 py-2 text-sm font-normal font-tertiary hover:text-primary transition"
+                className="px-4 py-2 text-sm hover:text-primary transition"
               >
                 Cómo colaborar
               </Link>
@@ -125,7 +151,7 @@ const Navbar = () => {
         </div>
         <span className="border-r border-2 h-10 border-primary" />
 
-        {/* Resto de Opciones */}
+        {/* Otras Vistas */}
         <Link to="/CuidadosMascota" className="hover:text-primary transition">
           Cuidados de tu mascota
         </Link>
@@ -135,44 +161,85 @@ const Navbar = () => {
         </Link>
       </nav>
 
-      {/* Botón Iniciar Sesión Desktop */}
+      {/* Botón Desktop */}
       <div className="hidden md:block">
-        <button
-          onClick={() => setLoginOpen(true)}
-          className="ml-4 border border-primary px-4 py-2 rounded-full font-bold hover:bg-orange-100 transition"
-        >
-          Iniciar sesión
-        </button>
+        {loggedIn ? (
+          <div
+            className="relative inline-block"
+            onMouseEnter={handleMouseEnterUser}
+            onMouseLeave={handleMouseLeaveUser}
+          >
+            <button className="flex items-center border border-primary px-4 py-2 rounded-full hover:bg-orange-50 transition">
+              <User size={20} className="mr-2 stroke-primary" />
+              <span>{user.fullname}</span>
+              <ChevronDown size={16} className="ml-2 stroke-primary" />
+            </button>
+            {isUserMenuOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg flex flex-col z-50">
+                <Link
+                  to="/profile"
+                  className="px-4 py-2 text-sm hover:bg-gray-100 transition"
+                >
+                  Actualizar información
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="w-full text-left px-4 py-2 text-sm hover:bg-gray-100 transition"
+                >
+                  Cerrar sesión
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <button
+            onClick={() => setLoginOpen(true)}
+            className="ml-4 border border-primary px-4 py-2 rounded-full font-bold hover:bg-orange-100 transition"
+          >
+            Iniciar sesión
+          </button>
+        )}
       </div>
 
       {/* Mobile */}
       {menuOpen && (
         <div className="absolute top-24 left-6 right-6 bg-white border border-primary rounded-2xl p-6 shadow-md flex flex-col items-center gap-4 z-50 md:hidden">
           {/* Inicio */}
-          <button
-            onClick={() => setIsInicioOpen(!isInicioOpen)}
-            className="w-full flex items-center justify-center hover:text-primary transition cursor-pointer"
-          >
-            <span>Inicio</span>
-            <ChevronDown
-              size={16}
-              className={`ml-1 transition-transform ${
-                isInicioOpen ? "rotate-180" : ""
-              }`}
-            />
-          </button>
-          {isInicioOpen && (
-            <div className="flex flex-col items-center gap-2 mt-2">
-              <Link
-                to="/#seguimiento"
-                className="hover:text-primary transition"
+          {loggedIn ? (
+            <>
+              <button
+                onClick={() => setIsInicioOpen(!isInicioOpen)}
+                className="w-full flex items-center justify-center hover:text-primary transition cursor-pointer"
               >
-                Seguimiento de tu match
-              </Link>
-              <Link to="/#historias" className="hover:text-primary transition">
-                Historias de adopción
-              </Link>
-            </div>
+                <span>Inicio</span>
+                <ChevronDown
+                  size={16}
+                  className={`ml-1 transition-transform ${
+                    isInicioOpen ? "rotate-180" : ""
+                  }`}
+                />
+              </button>
+              {isInicioOpen && (
+                <div className="flex flex-col items-center gap-2 mt-2">
+                  <Link
+                    to="/#seguimiento"
+                    className="hover:text-primary transition"
+                  >
+                    Seguimiento de tu match
+                  </Link>
+                  <Link
+                    to="/#historias"
+                    className="hover:text-primary transition"
+                  >
+                    Historias de adopción
+                  </Link>
+                </div>
+              )}
+            </>
+          ) : (
+            <Link to="/" className="hover:text-primary transition">
+              Inicio
+            </Link>
           )}
 
           {/* Nosotros */}
@@ -211,18 +278,52 @@ const Navbar = () => {
             </div>
           )}
 
+          {/* Otras Vistas */}
           <Link to="/CuidadosMascota" className="hover:text-primary transition">
             Cuidados de tu mascota
           </Link>
           <Link to="/Contacto" className="hover:text-primary transition">
             Contacto
           </Link>
-          <button
-            onClick={() => setLoginOpen(true)}
-            className="mt-2 border border-primary px-4 py-2 rounded-full cursor-pointer font-bold hover:bg-orange-100 transition"
-          >
-            Iniciar sesión
-          </button>
+
+          {/* Botón mobile */}
+          {loggedIn ? (
+            <>
+              <button
+                onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
+                className="w-full flex items-center justify-center hover:text-primary transition cursor-pointer"
+              >
+                <User size={20} className="mr-2 stroke-primary" />
+                <span>{user.fullname}</span>
+                <ChevronDown
+                  size={16}
+                  className={`ml-1 transition-transform ${
+                    isUserMenuOpen ? "rotate-180" : ""
+                  }`}
+                />
+              </button>
+              {isUserMenuOpen && (
+                <div className="flex flex-col items-center gap-2 mt-2">
+                  <Link to="/profile" className="hover:text-primary transition">
+                    Actualizar información
+                  </Link>
+                  <button
+                    onClick={handleLogout}
+                    className="hover:text-primary transition"
+                  >
+                    Cerrar sesión
+                  </button>
+                </div>
+              )}
+            </>
+          ) : (
+            <button
+              onClick={() => setLoginOpen(true)}
+              className="mt-2 border border-primary px-4 py-2 rounded-full font-bold hover:bg-orange-100 transition"
+            >
+              Iniciar sesión
+            </button>
+          )}
         </div>
       )}
 

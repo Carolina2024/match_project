b0a9066f40a5bd2beda98dd40685b4a81761cad0
Angel Añez <angdavid2910@gmail.com>
2025-05-13 19:06:58 -0400

    feat: add rate limit at recoverPassword and sendContactMessage endpoints

--- backend/src/module/mail/mail.controller.ts
+++ backend/src/module/mail/mail.controller.ts
@@ -1,7 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { ContactMessageDto } from './dto/contact-message.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiTooManyRequestsResponse,
+} from '@nestjs/swagger';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('mail')
 export class MailController {
@@ -12,6 +17,21 @@ export class MailController {
     description:
       'Permite enviar a través de correo electrónico un mensaje de contacto a la fundación.',
   })
+  @ApiOkResponse({
+    description: 'El correo es enviado exitosamente',
+    example: {
+      message:
+        'Te hemos enviado un correo con un enlace y un código para recuperar tu contraseña',
+    },
+  })
+  @ApiTooManyRequestsResponse({
+    description: 'El usuario hace muchas peticiones al endpoint',
+    example: {
+      statusCode: 429,
+      message: 'ThrottlerException: Too Many Requests',
+    },
+  })
+  @UseGuards(ThrottlerGuard)
   @Post('contact')
   async sendContactMessage(@Body() contactMessageDto: ContactMessageDto) {
     await this.mailService.sendContactMessage(contactMessageDto);

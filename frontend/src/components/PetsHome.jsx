ef4f5f3b87146294c46b7e5c8597271d9d2da86f
Luis Angel <angel.quispe.navarro@outlook.com>
2025-05-17 22:32:24 -0500

    Component Pets Compatibilits Responsive

--- frontend/src/components/PetsHome.jsx
+++ frontend/src/components/PetsHome.jsx
@@ -1,15 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import { usePet } from "../context/PetContext";
 
 function PetsHome() {
-  const { matchedPet, isMatched, handleClickConoceme, mascotas } = usePet();
+  const { matchedPet, handleClickConoceme, mascotas } = usePet();
 
-  function noHagoNada() {
-
-  }
-
-  const navigate = useNavigate();
-
+  function noHagoNada() {}
 
   return (
     <div className="mx-auto bg-transparent">
@@ -18,37 +12,37 @@ function PetsHome() {
       </h2>
       <hr className="w-3/5 border-t-1 border-primary mx-auto mt-4 md:mb-10 mb-10" />
       <div className="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-4 gap-1 h-full bg-transparent">
-        {mascotas.map(
-          (mascota) => (
-            <div key={mascota.id} className="p-4 items-center text-center">
-              <img
-                src={mascota.photoUrls[0]}
-                alt={mascota.name}
-                className="md:w-55 md:h-55 mx-auto object-cover bg-contain md:rounded-[80px] rounded-full hover:scale-105 transition mb-2"
-              />
-              <div className="flex flex-col gap-3 items-center">
-                <div>
-                  <h3 className="text-center md:text-2xl text-xl font-normal mt-2 mb-1">
-                    {mascota.name}, {mascota.sex}
-                  </h3>
-                  <p className="font-extrabold md:text-2xl text-lg font-tertiary text-primary">{mascota.traits[0]}</p>
-                </div>
-                <div className="flex flex-col mt-2">
-                  <button
-                    onClick={() =>
-                      matchedPet?.photoUrls !== undefined
-                        ? noHagoNada()
-                        : handleClickConoceme(mascota)
-                    }
-                    className="border border-primary rounded-4xl shadow-lg cursor-pointer md:text-xl text-base px-6 py-2 text-white font-bold bg-primary hover:scale-105 transition"
-                  >
-                    Conóceme
-                  </button>
-                </div>
+        {mascotas.map((mascota) => (
+          <div key={mascota.id} className="p-4 items-center text-center">
+            <img
+              src={mascota.photoUrls[0]}
+              alt={mascota.name}
+              className="md:w-55 md:h-55 mx-auto object-cover bg-contain md:rounded-[80px] rounded-full hover:scale-105 transition mb-2"
+            />
+            <div className="flex flex-col gap-3 items-center">
+              <div>
+                <h3 className="text-center md:text-2xl text-xl font-normal mt-2 mb-1">
+                  {mascota.name}, {mascota.sex}
+                </h3>
+                <p className="font-extrabold md:text-2xl text-lg font-tertiary text-primary">
+                  {mascota.traits[0]}
+                </p>
+              </div>
+              <div className="flex flex-col mt-2">
+                <button
+                  onClick={() =>
+                    matchedPet?.photoUrls !== undefined
+                      ? noHagoNada()
+                      : handleClickConoceme(mascota)
+                  }
+                  className="border border-primary rounded-4xl shadow-lg cursor-pointer md:text-xl text-base px-6 py-2 text-white font-bold bg-primary hover:scale-105 transition"
+                >
+                  Conóceme
+                </button>
               </div>
             </div>
-          )
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );

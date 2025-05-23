c5fd3b2fcb64b0eed82832d839ec7c8f6c6cfc81
Angel Añez <angdavid2910@gmail.com>
2025-05-23 14:56:13 -0400

    refactor(users): reorganize Swagger decorators in a single docs file

--- backend/src/module/users/users.controller.ts
+++ backend/src/module/users/users.controller.ts
@@ -7,259 +7,42 @@ import {
   ParseUUIDPipe,
   Patch,
   Query,
-  UseGuards,
 } from '@nestjs/common';
 
 import { UsersService } from './users.service';
-import {
-  ApiOkResponse,
-  ApiOperation,
-  ApiNotFoundResponse,
-  ApiUnauthorizedResponse,
-  ApiForbiddenResponse,
-  ApiBearerAuth,
-  ApiConflictResponse,
-  ApiBadRequestResponse,
-} from '@nestjs/swagger';
 import { GetUsersQueryDto } from './dtos/get-users-query.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { UserRole } from 'src/common/enums/userRole.enum';
-import { OwnerOrAdminGuard } from '../auth/guards/owner-or-admin.guard';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import {
+  DeleteUserDocs,
+  GetAllUsersDocs,
+  GetUserByIdDocs,
+  UpdateUserByIdDocs,
+} from './users.controller.docs';
+import { OwnerOrAdminAuth } from '../auth/decorators/owner-or-admin-auth.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @ApiOperation({
-    summary: 'Obtener todos los usuarios',
-    description:
-      'Retorna un listado de todos los usuarios registrados en la plataforma (solo para administradores)',
-  })
-  @ApiOkResponse({
-    description: 'Retorno del listado de usuarios',
-    example: {
-      data: [
-        {
-          id: '639dcdc7-a635-48d4-a641-2c74d0878bbd',
-          fullname: 'José Gómez',
-          email: 'jose@example.com',
-          role: 'adoptante',
-          createdAt: '2025-05-16T03:47:02.989Z',
-          adopter: {
-            id: '4aa3fc42-7b20-4058-a5d6-602e73b99a10',
-            identityDocument: '28631246-9',
-            birthDate: '2006-11-18',
-            address: 'Calle 15, Urb Caja Grande, Región Metropolitana',
-            homeType: 'Departamento pequeño',
-            allowsPets: true,
-            hadPets: false,
-            hadPetsVaccinated: false,
-            hadPetsCastrated: false,
-            hoursAlone: 7,
-            petDestroy:
-              'Me pondría muy triste y trataría de prestarle más atención para ver por qué hace esas cosas',
-            preparedToVisitVeterinarian: false,
-            allowsVisit: true,
-            isResponsibleAdoption: true,
-            userPreferenceEnergy: 'Moderado',
-            userPreferenceTraits: ['Cariñoso', 'Juguetón'],
-            userPreferenceDogs: false,
-            userPreferenceCats: true,
-            userPreferenceChildren: false,
-          },
-        },
-        {
-          id: '2d00a0bd-03dc-4e2a-8a39-12e54b5cacc5',
-          fullname: 'John Doe',
-          email: 'john@example.com',
-          role: 'adoptante',
-          createdAt: '2025-05-16T03:47:02.989Z',
-          adopter: {
-            id: 'da6c521a-fdb1-429d-8c68-deb8805172e2',
-            identityDocument: '12345678-9',
-            birthDate: '2000-05-09',
-            address: 'Calle 13, Departamento 2, Región Metropolitana',
-            homeType: 'Departamento grande',
-            allowsPets: true,
-            hadPets: false,
-            hadPetsVaccinated: false,
-            hadPetsCastrated: false,
-            hoursAlone: 2,
-            petDestroy: 'Lo educaré para que no lo vuelva a hacer',
-            preparedToVisitVeterinarian: false,
-            allowsVisit: true,
-            isResponsibleAdoption: true,
-            userPreferenceEnergy: 'Tranquilo',
-            userPreferenceTraits: ['Cariñoso', 'Juguetón'],
-            userPreferenceDogs: false,
-            userPreferenceCats: true,
-            userPreferenceChildren: false,
-          },
-        },
-      ],
-      total: 2,
-      page: 1,
-      limit: 10,
-      totalPages: 1,
-    },
-  })
-  @Auth(UserRole.ADMIN)
   @Get()
+  @Auth(UserRole.ADMIN)
+  @GetAllUsersDocs()
   getAllUsers(@Query() query: GetUsersQueryDto) {
     return this.usersService.findAll(query);
   }
 
-  @ApiBearerAuth()
-  @ApiOperation({
-    summary: 'Obtener un usuario por su ID',
-    description:
-      'Retorna los datos del usuario con el ID indicado (solo para administradores o adoptantes con su propio ID)',
-  })
-  @ApiOkResponse({
-    description: 'Se devuelve el usuario con el ID indicado',
-    example: {
-      id: '639dcdc7-abcd-1234-a641-2c74d087fghj',
-      fullname: 'José Gómez',
-      email: 'jose@example.com',
-      role: 'adoptante',
-      createdAt: '2025-05-16T03:47:02.989Z',
-      adopter: {
-        id: '4aa3fc42-4321-4dac-a5d6-602e73b99abc',
-        identityDocument: '28631246-9',
-        birthDate: '2006-11-18',
-        address: 'Calle 15, Urb Caja Grande, Región Metropolitana',
-        homeType: 'Departamento pequeño',
-        allowsPets: true,
-        hadPets: false,
-        hadPetsVaccinated: false,
-        hadPetsCastrated: false,
-        hoursAlone: 7,
-        petDestroy:
-          'Me pondría muy triste y trataría de prestarle más atención para ver por qué hace esas cosas',
-        preparedToVisitVeterinarian: false,
-        allowsVisit: true,
-        isResponsibleAdoption: true,
-        userPreferenceEnergy: 'Moderado',
-        userPreferenceTraits: ['Cariñoso', 'Juguetón'],
-        userPreferenceDogs: false,
-        userPreferenceCats: true,
-        userPreferenceChildren: false,
-      },
-    },
-  })
-  @ApiUnauthorizedResponse({
-    description: 'El usuario no está autenticado',
-    example: {
-      message: 'Unauthorized',
-      statusCode: 401,
-    },
-  })
-  @ApiForbiddenResponse({
-    description: 'Usuario sin permisos para acceder al recurso',
-    example: {
-      message: 'Usuario sin permisos suficientes',
-      error: 'Forbidden',
-      statusCode: 403,
-    },
-  })
-  @ApiNotFoundResponse({
-    description: 'No se ha encontrado un usuario con el ID indicado',
-    example: {
-      message:
-        'Usuario con id 639dcdc7-a635-48d4-a641-2c74d0878cbd no encontrado',
-      error: 'Not Found',
-      statusCode: 404,
-    },
-  })
-  @UseGuards(AuthGuard(), OwnerOrAdminGuard)
   @Get(':id')
+  @OwnerOrAdminAuth()
+  @GetUserByIdDocs()
   getUserById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOneById(id);
   }
 
-  @ApiBearerAuth()
-  @ApiOperation({
-    summary: 'Actualizar un usuario por su ID',
-    description:
-      'Permite actualizar uno o más campos de un usuario por su ID (solo para administradores o adoptantes con su propio ID)',
-  })
-  @ApiOkResponse({
-    description: 'Se actualiza el usuario exitosamente',
-    example: {
-      id: '2d00a0bd-03dc-4e2a-8a39-12e54b5cacc5',
-      fullname: 'John Miguel Doe',
-      email: 'johnmigueldoe@example.com',
-      role: 'adoptante',
-      adopter: {
-        id: 'da6c521a-fdb1-429d-8c68-deb8805172e2',
-        identityDocument: '12345678-9',
-        birthDate: '2000-05-09',
-        address: 'Calle 13, Departamento 2, Región Metropolitana',
-        homeType: 'Departamento grande',
-        allowsPets: true,
-        hadPets: false,
-        hadPetsVaccinated: false,
-        hadPetsCastrated: false,
-        hoursAlone: 2,
-        petDestroy: 'Lo educaré para que no lo vuelva a hacer',
-        preparedToVisitVeterinarian: false,
-        allowsVisit: true,
-        isResponsibleAdoption: true,
-        userPreferenceEnergy: 'Tranquilo',
-        userPreferenceTraits: ['Cariñoso', 'Juguetón'],
-        userPreferenceDogs: true,
-        userPreferenceCats: true,
-        userPreferenceChildren: false,
-      },
-    },
-  })
-  @ApiBadRequestResponse({
-    description: 'El usuario ingresa un dato con un formato inválido',
-    example: {
-      message: [
-        'Ingrese un Documento de Identidad válido en Chile siguiendo el siguiente formato: 12345678-9',
-      ],
-      error: 'Bad Request',
-      statusCode: 400,
-    },
-  })
-  @ApiUnauthorizedResponse({
-    description: 'El usuario no está autenticado',
-    example: {
-      message: 'Unauthorized',
-      statusCode: 401,
-    },
-  })
-  @ApiForbiddenResponse({
-    description: 'Usuario sin permisos para acceder al recurso',
-    example: {
-      message: 'Usuario sin permisos suficientes',
-      error: 'Forbidden',
-      statusCode: 403,
-    },
-  })
-  @ApiNotFoundResponse({
-    description: 'No se ha encontrado un usuario con el ID indicado',
-    example: {
-      message:
-        'Usuario con id 639dcdc7-a635-48d4-a641-2c74d0878cbd no encontrado',
-      error: 'Not Found',
-      statusCode: 404,
-    },
-  })
-  @ApiConflictResponse({
-    description:
-      'El correo electrónico o el documento de identidad ingresados por el usuario ya se encuentran registrados por otro usuario',
-    example: {
-      message: 'Ya existe un usuario con ese correo',
-      error: 'Conflict',
-      statusCode: 409,
-    },
-  })
-  @UseGuards(AuthGuard(), OwnerOrAdminGuard)
   @Patch(':id')
+  @OwnerOrAdminAuth()
+  @UpdateUserByIdDocs()
   updateUserById(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -267,28 +50,9 @@ export class UsersController {
     return this.usersService.updateById(id, updateUserDto);
   }
 
-  @ApiOperation({
-    summary: 'Elimina la cuenta del usuario por su ID',
-    description:
-      'Retorna un mensaje indicando que la cuenta del usuario se ha eliminado exitosamente (solo para administradores)',
-  })
-  @ApiOkResponse({
-    description: 'Se elimina la cuenta del usuario con el ID',
-    example: {
-      message: 'La cuenta del usuario ha sido eliminada exitosamente',
-    },
-  })
-  @ApiNotFoundResponse({
-    description: 'No se encuentra el usuario con el ID',
-    example: {
-      message:
-        'Usuario con id 639dcdc7-a635-48d4-b641-2c74d0878bbd no encontrado',
-      error: 'Not Found',
-      statusCode: 404,
-    },
-  })
-  @Auth(UserRole.ADMIN)
   @Delete(':id')
+  @Auth(UserRole.ADMIN)
+  @DeleteUserDocs()
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.removeById(id);
   }

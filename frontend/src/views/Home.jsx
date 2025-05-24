84e591b6b0d0e2bb4e88173a79f353de2b199274
Christian Martinez <dantedeat@hotmail.com>
2025-05-23 20:37:12 -0600

    refactor: convert inline SVGs in Home to reusable components

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -16,6 +16,12 @@ import { HandHeart } from "lucide-react";
 import PetsHome from "../components/PetsHome";
 import AuthModalsController from "../components/modals/AuthModalsController";
 import { useLocation, useNavigate } from "react-router-dom";
+import BackgroundShape from "../assets/icons/svg-components/BackgroundShape";
+import BackgroundUserActive from "../assets/icons/svg-components/BackgroundUserActive";
+import CurvedLine from "../assets/icons/svg-components/CurvedLine";
+import UserPlusIcon from "../assets/icons/svg-components/UserPlusIcon";
+import HomePreferenceIcon from "../assets/icons/svg-components/HomePreferenceIcon";
+import FindYourMatch from "../assets/icons/svg-components/FindYourMatch";
 
 const Home = () => {
   const [isLoginOpen, setLoginOpen] = useState(false);
@@ -97,22 +103,8 @@ const Home = () => {
         <>
           <div className="relative w-full md:max-w-6xl max-w-lg overflow-hidden md:flex items-center md:ml-10 bg-transparent">
 
-            <svg
-              width="1030"
-              height="745"
-              viewBox="0 0 1218 745"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="hidden md:block"
-            >
-              <rect
-                width="1218"
-                height="745"
-                rx="260"
-                fill="#F4A470"
-                fillOpacity="0.17"
-              />
-            </svg>
+<BackgroundShape />
+ 
 
             <div className="block md:hidden w-[400px] mt-10 ml-6 bg-[#F6E8D8] rounded-[80px] px-4 pt-10 pb-4 mb-16">
               <div className="flex flex-col items-center text-center">
@@ -228,21 +220,8 @@ const Home = () => {
           </div>
 
           <div className=" hidden md:flex relative w-full md:max-w-5xl max-w-lg mt-12 overflow-ellipsis items-center md:ml-10">
-            <svg
-              width="918"
-              height="425"
-              viewBox="0 0 918 425"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                width="918"
-                height="400"
-                rx="148.5"
-                fill="#F4A470"
-                fillOpacity="0.17"
-              />
-            </svg>
+
+<BackgroundUserActive />
 
             <div className="absolute inset-y-0 -mt-3 right-0 ">
               <img
@@ -289,26 +268,9 @@ const Home = () => {
           3 simples pasos
         </h2>
         <div className="hidden absolute top-50 left-0 right-0 md:flex justify-center">
-          <svg
-            width="650"
-            height="104"
-            viewBox="0 0 690 104"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.02832 2.06604C76.3448 -5.06727 64.2133 95.8158 211.308 102.185"
-              stroke="#F4A470"
-              strokeWidth="2"
-              strokeDasharray="10 5"
-            />
-            <path
-              d="M689.896 2.06604C614.58 -5.06727 626.712 95.8158 479.617 102.185"
-              stroke="#F4A470"
-              strokeWidth="2"
-              strokeDasharray="10 5"
-            />
-          </svg>
+
+<CurvedLine />
+
         </div>
         <div className="mt-10 flex flex-col md:flex-row items-center justify-center md:gap-50 gap-14 relative w-full max-w-6xl z-10">
           <div className="rounded-3xl border-[#DFDFDF] border shadow-md/25 px-1 py-6 flex flex-col items-center text-center md:w-[300px] w-[300px] h-[240px] relative">
@@ -316,34 +278,9 @@ const Home = () => {
               1
             </div>
             <div className="mt-10 flex items-center justify-center h-20">
-              <svg
-                width="80"
-                height="55"
-                viewBox="0 0 80 55"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M73.3333 17.5002V10.8335H66.6667V17.5002H60V24.1668H66.6667V30.8335H73.3333V24.1668H80V17.5002H73.3333Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M26.6667 27.5002C34.0333 27.5002 40 21.5335 40 14.1668C40 6.80016 34.0333 0.833496 26.6667 0.833496C19.3 0.833496 13.3333 6.80016 13.3333 14.1668C13.3333 21.5335 19.3 27.5002 26.6667 27.5002ZM26.6667 7.50016C30.3333 7.50016 33.3333 10.5002 33.3333 14.1668C33.3333 17.8335 30.3333 20.8335 26.6667 20.8335C23 20.8335 20 17.8335 20 14.1668C20 10.5002 23 7.50016 26.6667 7.50016Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M26.6667 30.8335C17.7667 30.8335 0 35.3002 0 44.1668V54.1668H53.3333V44.1668C53.3333 35.3002 35.5667 30.8335 26.6667 30.8335ZM46.6667 47.5002H6.66667V44.2002C7.33333 41.8002 17.6667 37.5002 26.6667 37.5002C35.6667 37.5002 46 41.8002 46.6667 44.1668V47.5002Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M41.7 1.00016C44.7667 4.5335 46.6667 9.1335 46.6667 14.1668C46.6667 19.2002 44.7667 23.8002 41.7 27.3335C48.2333 26.5002 53.3333 20.9668 53.3333 14.1668C53.3333 7.36683 48.2333 1.8335 41.7 1.00016Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M55.1 33.6002C58.0667 36.3668 60 39.8335 60 44.1668V54.1668H66.6667V44.1668C66.6667 39.3335 61.3667 35.8002 55.1 33.6002Z"
-                  fill="#F4A470"
-                />
-              </svg>
+
+<UserPlusIcon />
+
             </div>
             <p className="mt-4 text-sm text-[#0C0C0C] font-primary font-normal">
               Completar el formulario de registro en unos minutos
@@ -355,34 +292,9 @@ const Home = () => {
               2
             </div>
             <div className="mt-10 flex items-center justify-center h-20">
-              <svg
-                width="74"
-                height="61"
-                viewBox="0 0 74 61"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0.333344 26.923V60.2563H20.3333V43.5897H27V60.2563H47V26.923L23.6667 10.2563L0.333344 26.923ZM40.3333 53.5897H33.6667V36.923H13.6667V53.5897H7.00001V30.3563L23.6667 18.4563L40.3333 30.3563V53.5897Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M60.3333 13.5897H53.6667V20.2563H60.3333V13.5897Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M60.3333 26.923H53.6667V33.5897H60.3333V26.923Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M60.3333 40.2563H53.6667V46.923H60.3333V40.2563Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M30.3333 0.256348V6.82301L37 11.5897V6.92301H67V53.5897H53.6667V60.2563H73.6667V0.256348H30.3333Z"
-                  fill="#F4A470"
-                />
-              </svg>
+
+ <HomePreferenceIcon />
+
             </div>
             <p className="mt-4 text-sm text-[#0C0C0C] font-primary font-normal">
               Describir tu hogar y tus preferencias para que Patas Pirque pueda
@@ -395,22 +307,9 @@ const Home = () => {
               3
             </div>
             <div className="mt-10 flex items-center justify-center h-20">
-              <svg
-                width="68"
-                height="75"
-                viewBox="0 0 68 75"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7.33335 14.1668H14V24.1668H47.3334V14.1668H54V30.8335H60.6667V14.1668C60.6667 10.5002 57.6667 7.50016 54 7.50016H40.0667C38.6667 3.6335 35 0.833496 30.6667 0.833496C26.3334 0.833496 22.6667 3.6335 21.2667 7.50016H7.33335C3.66669 7.50016 0.666687 10.5002 0.666687 14.1668V60.8335C0.666687 64.5002 3.66669 67.5002 7.33335 67.5002H24V60.8335H7.33335V14.1668ZM30.6667 7.50016C32.5 7.50016 34 9.00016 34 10.8335C34 12.6668 32.5 14.1668 30.6667 14.1668C28.8334 14.1668 27.3334 12.6668 27.3334 10.8335C27.3334 9.00016 28.8334 7.50016 30.6667 7.50016Z"
-                  fill="#F4A470"
-                />
-                <path
-                  d="M58.3334 60.5002C59.6667 58.1668 60.6667 55.5002 60.6667 52.5002C60.6667 44.1668 54 37.5002 45.6667 37.5002C37.3334 37.5002 30.6667 44.1668 30.6667 52.5002C30.6667 60.8335 37.3334 67.5002 45.6667 67.5002C48.6667 67.5002 51.3334 66.5002 53.6667 65.1668L62.6667 74.1668L67.3334 69.5002L58.3334 60.5002ZM45.6667 60.8335C41 60.8335 37.3334 57.1668 37.3334 52.5002C37.3334 47.8335 41 44.1668 45.6667 44.1668C50.3334 44.1668 54 47.8335 54 52.5002C54 57.1668 50.3334 60.8335 45.6667 60.8335Z"
-                  fill="#F4A470"
-                />
-              </svg>
+
+              <FindYourMatch />
+
             </div>
             <p className="mt-4 text-sm text-[#0C0C0C] font-primary font-normal">
               Encontrar a tu Match
